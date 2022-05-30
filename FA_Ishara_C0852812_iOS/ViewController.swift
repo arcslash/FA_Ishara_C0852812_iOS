c7bd9234aa9c6ed200730b625fbea83bf613df6b
Ishara Abeykoon <isharaux@gmail.com>
2022-05-30 11:00:34 -0400

    Initial Commit

---
+++ FA_Ishara_C0852812_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FA_Ishara_C0852812_iOS
+//
+//  Created by Ishara Abeykoon on 2022-05-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FA_Ishara_C0852812_iOSUITests/FA_Ishara_C0852812_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FA_Ishara_C0852812_iOSUITestsLaunchTests.swift
+//  FA_Ishara_C0852812_iOSUITests
+//
+//  Created by Ishara Abeykoon on 2022-05-30.
+//
+
+import XCTest
+
+class FA_Ishara_C0852812_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
